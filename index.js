a2be96f11c30ea0d22d3e5c89dba633a1a6492e6
Matt Bullen <burner0110@gmail.com>
2015-09-02 20:26:19 -0700

    .

--- index.js
+++ index.js
@@ -7,7 +7,9 @@
         jsdom = require("jsdom").jsdom;
     
     var window = jsdom().parentWindow;
-
+    
+    var _SAVED = 0;
+    
     var server = http.createServer(function (req, res) {
     
         if (req.url == "/" || req.url == "/index.html") {
@@ -160,27 +162,21 @@
             var script = 'd3.select("#line").on("mousemove", function() { var x = d3.scale.linear().domain([0, ' + (samples.length - 1) + ']).range([0, ' + width + ']); var y = d3.scale.linear().domain([' + d3.min(samples) + ', ' + d3.max(samples) + ']).range([' + height + ', 0]); var xMouse = x.invert(d3.mouse(this)[0]).toFixed(3); var yMouse = y.invert(d3.mouse(this)[1]).toFixed(3); console.log("x: " + xMouse + " y: " + yMouse); var tooltip = d3.select("#line-tooltip"); tooltip.html("x: " + xMouse + ", y: " + yMouse); tooltip.style({"position": "absolute", "outline": "none", "background": "rgb(6, 12, 124)", "font-family": "Arial, sans-serif", "-webkit-font-smoothing": "subpixel-antialiased", "color": "#fff", "font-size": "12px", "line-height": "1.5", "border-radius": "5px", "box-sizing": "border-box", "min-width": "50px", "padding": "8px 16px", "z-index": "9999"}); var pageXValue = d3.event.pageX + 3; var pageYValue = d3.event.pageY - 35; tooltip.style({"left": "" + pageXValue + "px", "top": "" + pageYValue + "px"}); tooltip.transition().duration(300).style("opacity", "1.0"); }).on("mouseout", function() { d3.select("#line-tooltip").transition().duration(300).style("opacity", "0"); d3.select("#line-tooltip").style("padding", "0").style("border", "0").html(""); });';
             svg.append("script").html(script);
             
-            /*
-            var filePath = path.join(__dirname, "index.html");
+            var filePath = path.join(__dirname, "saved-" + _SAVED + ".html");
             fs.writeFile(filePath, '<script src="./d3.min.js"> </script><div style="width: 600px; margin: 0 auto;"><h3 style="width: 100%; margin-top: 5rem; margin-bottom: 36px; padding-left: 18px; font-family: Arial, sans-serif; font-size: 24px; text-align: center;">Server-rendered interactive D3.js SVG</h3>' + virtualDiv.innerHTML + '</div>', function(error) {
                 if (!error) {
-                    res.writeHead(200, {"Content-Type": "text/html"});           
-                    var readStream = fs.createReadStream(filePath);
-                    readStream.pipe(res);
+                    // res.writeHead(200, {"Content-Type": "text/html"});           
+                    // var readStream = fs.createReadStream(filePath);
+                    // readStream.pipe(res);
+                    _SAVED++;
                 } else {
-                    console.log("Error:", error);
+                    console.log("File Write Error:", error);
                     res.end();
                 }
             });
-            */
-            res.writeHead(200, {"Content-Type": "text/html"});
-            res.write('<script src="./d3.min.js"> </script><div style="width: 600px; margin: 0 auto;"><h3 style="width: 100%; margin-top: 5rem; margin-bottom: 36px; padding-left: 18px; font-family: Arial, sans-serif; font-size: 24px; text-align: center;">Server-rendered interactive D3.js SVG</h3>' + virtualDiv.innerHTML + '</div>');
-            //res.write('<script src="./d3.min.js"> </script>'
-            //+ "<script>window.onload = function() { document.body.innerHTML =" + virtualDiv.innerHTML + "; }</script>");
-            //+ '<div style="width: 600px; margin: 0 auto;"><h3 style="width: 100%; margin-top: 5rem; margin-bottom: 36px; padding-left: 18px; font-family: Arial, sans-serif; //font-size: 24px; text-align: center;">Server-rendered interactive D3.js SVG</h3>'
-            //+ virtualDiv.innerHTML
-            //+ "</div>'; };</script>");
             
+            res.writeHead(200, {"Content-Type": "text/html"});
+            res.write('<script src="./d3.min.js"> </script><div style="width: 600px; margin: 0 auto;"><h3 style="width: 100%; margin-top: 5rem; margin-bottom: 36px; padding-left: 18px; font-family: Arial, sans-serif; font-size: 24px; text-align: center;">Server-rendered interactive D3.js SVG</h3>' + virtualDiv.innerHTML + '</div>');         
             res.end();
             
         } else {
